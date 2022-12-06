3fc9cb59e2cd0e0bb28f53caff4aa5506da2f60e
hglfs <a2352301708@163.com>
2022-12-06 15:07:44 +0800

    router 08

--- note/05_路由别名和重定向/components/B.vue
+++ note/05_路由别名和重定向/components/B.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>C</h1>
+  <h1>B</h1>
 </template>
 <script setup lang="ts">
 
---
+++ src/components/root.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Root</h1>
+  <router-view></router-view>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped lang="less">
+
+</style>
